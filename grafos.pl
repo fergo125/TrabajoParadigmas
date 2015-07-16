597a8924f7a7e53549966a602ba7bb877dc90257
fergo125 <ffmm.14@gmail.com>
2015-07-15 18:02:50 -0600

    Agregadas funciones de grafos para analizar los arboles

--- grafos.pl
+++ grafos.pl
@@ -1,2 +1,14 @@
 %grafo([a,b,c,d,e,g,h],[arco(a,b), arco(a,d),
-% arco(b,d),arco(d,g,)marco(e,h)],C).
+% arco(b,d),arco(d,g,)arco(e,h)]).
+:-[utiles].
+:-[proclengnat]
+grafo([[],[]]).
+concatenar([],L,L).
+concatenar(E,L,R):-
+	miembro(L,E),!,concatenar([],L|E,R).
+agregarAGrafo(E,Hd|_):-
+	concatenar(E,Hd).
+agregarArcoGrafo(E1,E2):-
+	assert(arco(E1,E2).
+
+
